387d6b7d756532ab882d296d9834bbab5b6aed81
metakat <metakatz@gmail.com>
2025-02-28 16:56:29 +0900

    fix paths

--- src/ui/assets/sources/ArchitectureKitSource.js
+++ src/ui/assets/sources/ArchitectureKitSource.js
@@ -5,7 +5,7 @@ export default class ArchitectureKitSource extends KitSource {
   constructor(api) {
     super(
       api,
-      "https://assets-prod.reticulum.io/kits/architecture/ArchKit-64274f78e194a993850e208cbaa2fe7c5a35a955.gltf"
+      "https://cyzyspace-public-assets.s3.us-east-1.amazonaws.com/spoke/ArchKit-64274f78e194a993850e208cbaa2fe7c5a35a955.gltf"
     );
     this.id = "architecture-kit";
     this.name = "Architecture Kit";
--- src/ui/assets/sources/RockKitSource.js
+++ src/ui/assets/sources/RockKitSource.js
@@ -3,7 +3,7 @@ import { TransformPivot } from "../../../editor/controls/SpokeControls";
 
 export default class RockKitSource extends KitSource {
   constructor(api) {
-    super(api, "https://assets-prod.reticulum.io/kits/rock/RocksKit_v2-f259c85a5e6091bd72eac7e2fdca177209613f1b.gltf");
+    super(api, "https://cyzyspace-public-assets.s3.us-east-1.amazonaws.com/spoke/RocksKit_v2-f259c85a5e6091bd72eac7e2fdca177209613f1b.gltf");
     this.id = "rock-kit";
     this.name = "Rock Kit";
     this.transformPivot = TransformPivot.Selection;
